bec7b741c3947d4aeff5c8a21a5844ceca71fadc
JGong7 <97704595+JGong7@users.noreply.github.com>
2022-02-03 22:45:28 -0800

    update sd

--- sd.java
+++ sd.java
@@ -1,2 +1,5 @@
 public class sd{
+    public static int division(int a, int b){
+        return a/b;
+    }
 }
---
+++ test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class test {
+    @Test 
+    public void testDivision(){
+        assertEquals(4, sd.division(6,3));
+    }
+}
